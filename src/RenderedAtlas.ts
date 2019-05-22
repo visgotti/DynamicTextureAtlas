bdc03e5934a06cc2e2a519e2c06183554205af41
visgotti <joevisca2@gmail.com>
2019-05-22 16:15:40 -0600

    reverted to old optimal addSprite function with fixed render method call'

--- src/RenderedAtlas.ts
+++ src/RenderedAtlas.ts
@@ -6,8 +6,7 @@ import { MultiAtlas } from './';
 export default class RenderedAtlas extends MultiAtlas {
     private renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer;
     private atlasRenderTextures: {[atlasIndex: number]: PIXI.RenderTexture } = {};
-    private mappedTextures: any = {};
-    private mappedSprites: {[id: string]: Array<PIXI.Sprite> } = {};
+    private mappedTextures: {[ textureId: string]: PIXI.Texture };
     constructor(renderer, maxAtlasWidth, maxAtlasHeight, expectedMinTextureLength)
     {
         super(maxAtlasWidth, maxAtlasHeight, expectedMinTextureLength);
@@ -21,17 +20,12 @@ export default class RenderedAtlas extends MultiAtlas {
      * @param sprite - sprite that has a texture we want to merge into our atlas
      * @param deleteSprite - if we want to delete the sprite and its textures after
      */
-    public addSprite (id, sprite: PIXI.Sprite) : PIXI.Texture {
-        // the mapped texture already exists now check to see
-        // if we need a new mapped sprite array.
+    public addSprite (id, sprite: PIXI.Sprite, deleteSprite=false) : PIXI.Texture {
         if(this.mappedTextures[id]) {
-            if(this.mappedSprites[id]) {
-                this.mappedSprites[id].push(sprite);
-            } else {
-                this.mappedSprites[id] = [sprite];
+            if(deleteSprite) {
+                sprite.destroy({children: true, texture: true, baseTexture: true});
             }
-            sprite.texture = this.mappedTextures[id];
-            return;
+            return this.mappedTextures[id];
         }
 
         const { atlasIndex, rect } = this.addRect(id, sprite.width, sprite.height);
@@ -40,75 +34,26 @@ export default class RenderedAtlas extends MultiAtlas {
             this.atlasRenderTextures[atlasIndex] = PIXI.RenderTexture.create(this.maxAtlasWidth, this.maxAtlasHeight);
         }
 
-        const container = new PIXI.Container();
-        const oldRenderTextureSprite = new PIXI.Sprite(this.atlasRenderTextures[atlasIndex]);
-        oldRenderTextureSprite.position.x = 0;
-        oldRenderTextureSprite.position.y = 0;
-
-        container.addChild(oldRenderTextureSprite);
-
-        const copiedSprite = new PIXI.Sprite(sprite.texture);
-
-        container.addChild(copiedSprite);
-        // assign sprite position to the open rect
-        copiedSprite.position.x = rect.x;
-        copiedSprite.position.y = rect.y;
-
-        // create the new render texture
-        const newRenderTexture = PIXI.RenderTexture.create(this.maxAtlasWidth, this.maxAtlasHeight);
-        // render sprite to render texture
-        this.renderer.render(container, newRenderTexture);
-
-        container.removeChild(oldRenderTextureSprite);
-        oldRenderTextureSprite.destroy({ children: true, texture: true, baseTexture: true });
-
-        container.removeChild(copiedSprite);
-        copiedSprite.destroy({ children: true, texture: false, baseTexture: false });
-
-        // update current render texture
-        this.atlasRenderTextures[atlasIndex] = newRenderTexture;
-
-        container.destroy({ children: true, baseTexture: false, texture: false });
-
-        // add the sprite to lookup
-        this.mappedSprites[id] = [sprite];
-
-        // updates all sprites with new textures using new render texture as base texture
-        // also updates the mappedTextures
-        this.updateSpriteTextures(atlasIndex);
-    }
-
-    public removeSprite(id, sprite) {
-        const sprites = this.mappedSprites[id];
-        let len = sprites.length;
-        while(len--) {
-            if(sprites[len] === sprite) {
-                sprites.splice(len, 1);
-                if(!sprites.length) {
-                    delete this.mappedSprites[id];
-                }
-                return;
-            }
-        }
-    }
-
-    public updateSpriteTextures(atlasIndex) {
-        const rects = this.getMappedRects(atlasIndex);
         const renderTexture = this.atlasRenderTextures[atlasIndex];
-        for(let id in this.mappedSprites) {
-            const sprites = this.mappedSprites[id];
-            const rect = rects[id];
-            const texture = new PIXI.Texture(
-                renderTexture.baseTexture,
-                new PIXI.Rectangle(rect.x, rect.y, rect.width, rect.height),
-            );
-            this.mappedTextures[id] = texture;
-            let len = sprites.length;
-            while(len--) {
-                let sprite = sprites[len];
-                sprite.texture.destroy();
-                sprite.texture = texture;
-            }
+
+        // assign sprite position to the open rect
+        sprite.position.x = rect.x;
+        sprite.position.y = rect.y;
+
+        // render sprite to render texture
+        this.renderer.render(sprite, renderTexture, false);
+
+        // make a reference to the new texture using render texture as base.
+        const texture = new PIXI.Texture(
+            renderTexture.baseTexture,
+            new PIXI.Rectangle(rect.x, rect.y, rect.width, rect.height),
+        );
+
+        this.mappedTextures[id] = texture;
+
+        if(deleteSprite) {
+            sprite.destroy({children: true, texture: true, baseTexture: true});
         }
+        return texture;
     }
 }
